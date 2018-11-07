05ffdf8b11ed96d5aa306d6091a5a7a1ee5dff12
Arthur Demchenkov <spinal.by@gmail.com>
2018-11-07 04:10:44 +0300

    clockd.c: add @mainpage comment

--- clockd/clockd.c
+++ clockd/clockd.c
@@ -1,4 +1,54 @@
 /**
+ @mainpage
+
+ Introduction
+ ------------
+  Time management service is a subsystem to provide a common library for
+  all time-related (not alarms, though) services.
+
+ Structure
+ ---------
+  The following ASCII art describes the architecture.
+
+<pre>
++-----+  +-----+  +-----+
+|app 1|  |app 2|  |app 3| applications
+-------------------------  libtime.h
+       | libtime |
+       +---------+
+         ^ ^ ^
+         | | |
+   D-Bus | | |
+         | | |
+         v v v
+       +------+  D-Bus +---+
+daemon |clockd|<-------|csd|
+       +------+        +---+
+           |                 userland
+-------------------------------------
+           |                 kernel
+     \- systemtime
+     \- RTC (real time clock)
+     \- timezone
+</pre>
+
+ Components
+ ----------
+  \b clockd
+  - The Daemon (see clockd.c, rclockd.c, mainloop.c and server.c)
+
+  \b libtime
+  - API library
+
+  \b csd
+  - Cellular service daemon
+  - Sends "network time changed" signal when operator time/timezone has
+    been received from vellnet (when attached to network)
+ */
+
+#include "mainloop.h"
+
+/**
  * @brief clockd main body
  *
  * @file  clockd.c
@@ -6,8 +56,6 @@
  *
  * @copyright GNU GPLv2 or later
  */
-
-#include "mainloop.h"
 
 /**
  * This is clockd main pgm.
